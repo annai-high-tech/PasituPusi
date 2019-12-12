54df872bc9d7d0691690bc2d34d3ffc89452f354
Charlin Joe <charlinjoec@gmail.com>
2019-12-13 02:59:30 +0530

    Drawer menus added

--- app/src/main/java/com/aht/business/kirti/pasitupusi/ui/main/MainActivity.java
+++ app/src/main/java/com/aht/business/kirti/pasitupusi/ui/main/MainActivity.java
@@ -7,12 +7,19 @@ import com.aht.business.kirti.pasitupusi.R;
 import com.aht.business.kirti.pasitupusi.ui.login.LoginMainActivity;
 import com.aht.business.kirti.pasitupusi.ui.login.LoginType;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -30,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logoutButton = findViewById(R.id.buttonSignOut);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
@@ -61,18 +81,54 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void signOut() {
+
+        FirebaseAuth.getInstance().signOut();
+
+        Intent mainPage = new Intent(MainActivity.this, LoginMainActivity.class);
+        MainActivity.this.finish();
+        MainActivity.this.startActivity(mainPage);
+
+    }
     View.OnClickListener mOnClickSignoutListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
-            FirebaseAuth.getInstance().signOut();
-
-            Intent mainPage = new Intent(MainActivity.this, LoginMainActivity.class);
-            MainActivity.this.finish();
-            MainActivity.this.startActivity(mainPage);
+            signOut();
 
         }
     };
+
+    private NavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            processMenuItem(menuItem);
+
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+    };
+
+    private boolean processMenuItem(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_logout:
+                signOut();
+                break;
+
+            case R.id.nav_profile:
+                //changeFragments(new ScientificCalculatorFragment(selectedTheme));
+                break;
+
+            default:
+                break;
+        }
+
+        return true;
+    }
 
 
 }
